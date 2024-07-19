f40098a0cf2d3d6851fcf6a1e66348876abd08eb
Helber-Carvalho <helbercarvalho8@gmail.com>
2024-07-18 21:41:22 -0300

    mudei o fale conosco

---
+++ alerts.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var alertDiv = document.getElementById("alertDiv");
+
+    alertDiv.addEventListener("click", function() {
+        alert("fiz ainda nao ma, deu pregica!");
+    });
+});
